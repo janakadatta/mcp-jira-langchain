8e9e06de33670b47ffae4867c0b5a7fee38b6a91
janakadatta <164403825+janakadatta@users.noreply.github.com>
2026-09-09 09:27:40 +0530

    Update jira_confluence_codegen.py

--- jira_confluence_codegen.py
+++ jira_confluence_codegen.py
@@ -7,7 +7,7 @@ from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain.agents import create_agent
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI 
 
 
 
